bf6f8a0ffa0a915864427e48a849362221efb9f9
BATTINA <58685099+BVNAGABABU@users.noreply.github.com>
2023-11-09 19:33:11 +0530

    Create check_sftp_user.sh

---
+++ check_sftp_user.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+array_data=(user_1 user_2 user_3 user_4 user_5 user_6)
+available_users=()
+not_available_users=()
+
+for (( i=0; i<${#array_data[@]}; i++ ))
+do
+    output=$(id sftp_${array_data[$i]})
+    if [[ $output == *"uid"* ]]
+    then
+        #echo "${array_data[$i]} user id is available"
+        available_users+=(${array_data[$i]})
+    else
+        #echo "${array_data[$i]} user id is not available"
+        not_available_users+=(${array_data[$i]})
+    fi
+done
+
+echo "\n================================"
+echo "Available users list:"
+echo "================================"
+
+for (( j=0; j<${#available_users[@]}; j++ ))
+do
+    echo "${available_users[$j]}"
+done
+
+echo "\n==============================="
+echo "Not available users list:"
+echo "================================"
+for (( k=0; k<${#not_available_users[@]}; k++ ))
+do
+    echo "${not_available_users[$k]}"
+done
